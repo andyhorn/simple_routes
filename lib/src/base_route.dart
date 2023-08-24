e9759164f3eec22f255023b78b438ece192e783f
Andy Horn <andyjhorn@gmail.com>
2023-08-24 13:13:41 -0700

    Add PathData classes
    Add a sealed PathData class and two subclasses:
      - AtomicPath
      - SegmentedPath
    
    These two classes will be used to carry path data into the BaseRoute in a future commit.
    
    This is part of an effort to improve that path processing within BaseRoute instead of relying on users to properly enter root and non-root paths.

--- lib/src/base_route.dart
+++ lib/src/base_route.dart
@@ -1,6 +1,24 @@
 import 'package:simple_routes/src/child_route.dart';
 import 'package:simple_routes/src/utils.dart';
 
+sealed class PathData {
+  const PathData();
+}
+
+class AtomicPath extends PathData {
+  const AtomicPath.root() : path = '/';
+  AtomicPath.from(String? path) : path = path?.replaceAll('/', '') ?? '';
+
+  final String path;
+}
+
+class SegmentedPath extends PathData {
+  SegmentedPath(List<String> segments)
+      : segments = segments.map((s) => s.replaceAll('/', '')).toList();
+
+  final List<String> segments;
+}
+
 /// An abstract class to serve as the parent for all routes.
 abstract class BaseRoute {
   const BaseRoute();
