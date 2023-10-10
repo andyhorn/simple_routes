e8f85c8446fb0b6f3285ffb7e6b803ec1d80b4e9
Andy Horn <andyjhorn@gmail.com>
2023-10-09 21:50:21 -0700

    Add TestApp and test routing

---
+++ test/models/routes.dart
@@ -0,0 +1,40 @@
+import 'package:simple_routes/simple_routes.dart';
+
+class Path1 extends SimpleRoute {
+  const Path1();
+
+  @override
+  String get path => '/path1';
+}
+
+class Path2 extends SimpleRoute {
+  const Path2();
+
+  @override
+  String get path => '/path2';
+}
+
+enum TestRouteParams {
+  test,
+}
+
+class TestRouteData extends SimpleRouteData {
+  const TestRouteData(this.value);
+
+  final String value;
+
+  @override
+  String inject(String path) {
+    return path.setParam(TestRouteParams.test, value);
+  }
+}
+
+class DataPath extends DataRoute<TestRouteData> implements ChildRoute<Path2> {
+  const DataPath();
+
+  @override
+  String get path => withPrefix(TestRouteParams.test);
+
+  @override
+  Path2 get parent => const Path2();
+}
