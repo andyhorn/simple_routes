8ec7fade11f9b4bfae7354e6668da06239c8e331
Andy Horn <andyjhorn@gmail.com>
2023-11-24 23:06:29 -0700

    Add mock router and router provider widget

---
+++ test/mocks.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:mocktail/mocktail.dart';
+
+class MockGoRouter extends Mock implements GoRouter {
+  MockGoRouter() {
+    when(() => go(
+          any(),
+          extra: any(named: 'extra'),
+        )).thenReturn(null);
+    when(() => push(
+          any(),
+          extra: any(named: 'extra'),
+        )).thenAnswer(
+      (_) => Future.value(null),
+    );
+  }
+}
+
+class MockGoRouterProvider extends StatelessWidget {
+  const MockGoRouterProvider({
+    required this.goRouter,
+    required this.child,
+    Key? key,
+  }) : super(key: key);
+
+  final GoRouter goRouter;
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) => InheritedGoRouter(
+        goRouter: goRouter,
+        child: child,
+      );
+}
