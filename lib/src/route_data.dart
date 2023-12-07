36d57869d7629c6575a883cdb8d676e99d7daf39
Andy Horn <andyjhorn@gmail.com>
2023-12-06 22:11:50 -0700

    Remove generic from route data
    Remove the generic type from `SimpleRouteData` and the `extra` property
    
    This argument wasn't helpful and made it harder to understand

--- lib/src/route_data.dart
+++ lib/src/route_data.dart
@@ -1,11 +1,11 @@
-abstract class SimpleRouteData<E extends Object> {
+abstract class SimpleRouteData {
   const SimpleRouteData();
 
   /// Inject path parameters into the route.
   Map<Enum, String> get parameters => const {};
 
   /// Inject "extra" data into the route.
-  E? get extra => null;
+  Object? get extra => null;
 
   /// Inject query parameters into the route.
   Map<Enum, String?> get query => const {};
