b677b4371835d358f37060a73fb308f6bb00840b
Andy Horn <andyjhorn@gmail.com>
2024-05-10 23:20:31 -0700

    Rename SimpleDataRoute

--- lib/simple_routes.dart
+++ lib/simple_routes.dart
@@ -1,5 +1,5 @@
 library simple_routes;
 
 export 'src/route_data.dart' show SimpleRouteData;
-export 'src/routes.dart' show SimpleRoute, ChildRoute, DataRoute;
+export 'src/routes.dart' show SimpleRoute, ChildRoute, SimpleDataRoute;
 export 'package:simple_routes/src/extensions/extensions.dart';
