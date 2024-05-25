f612bcbd6d5f80371554173591530afaa8f5bd18
Andy <andyjhorn@gmail.com>
2024-05-25 11:26:14 -0700

    ref: Remove unused extensions (#30)
    * Remove unused extensions
    
    * Bump to 2.0.1
    
    * Update CHANGELOG

--- lib/simple_routes.dart
+++ lib/simple_routes.dart
@@ -2,4 +2,3 @@ library simple_routes;
 
 export 'src/route_data.dart' show SimpleRouteData;
 export 'src/routes.dart' show SimpleRoute, ChildRoute, SimpleDataRoute;
-export 'package:simple_routes/src/extensions/extensions.dart';
