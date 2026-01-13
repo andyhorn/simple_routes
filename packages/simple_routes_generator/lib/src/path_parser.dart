b64b1f792e283e4a2bc20160d4bb0b5d32912ddb
Andy Horn <andyjhorn@gmail.com>
2026-01-12 22:38:08 -0700

    chore: fix linter warning with ignor

--- packages/simple_routes_generator/lib/src/path_parser.dart
+++ packages/simple_routes_generator/lib/src/path_parser.dart
@@ -3,6 +3,8 @@ class PathParser {
   ///
   /// For example, `"/users/:userId/posts/:postId"` returns `["userId", "postId"]`.
   static List<String> parseParams(String path) {
+    // This is a bug in the Dart SDK
+    // ignore: deprecated_member_use
     final regExp = RegExp(r':([a-zA-Z0-9_]+)');
     return regExp.allMatches(path).map((m) => m.group(1)!).toList();
   }
